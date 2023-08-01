d46008d4a94daf739c394880f5d390c9656ad3a0
[Github <[Github>
2023-08-02 01:34:22 +0900

    3w 개발 중

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import MainContent
 
 def index(request):
@@ -12,3 +12,8 @@ def page(request):
     content_list=MainContent.objects.order_by('-pub_date')
     context={'content_list':content_list}
     return render(request,'mysite/content.html',context)
+
+def detail(request, content_id): 
+    content_list = get_object_or_404(MainContent, pk=content_id) 
+    context = { 'content_list': content_list} 
+    return render(request, 'mysite/content_detail.html', context)
